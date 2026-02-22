a397990833d5bd617ba01b110c600d73c09ca176
Dasango <dasango@uce.edu.ec>
2026-02-21 22:21:19 -0500

    No longer an expiration time during development

--- user-auth/src/main/java/com/auction/userfinance/web/security/JwtUtils.java
+++ user-auth/src/main/java/com/auction/userfinance/web/security/JwtUtils.java
@@ -11,8 +11,7 @@ import java.util.Date;
 public class JwtUtils {
 
     private final String SECRET_KEY = "secreto_super_seguro_100%_real_el_proyecto_de_apuestas";
-    
-    private final long EXPIRATION_TIME = 86400000;
+
 
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
@@ -22,7 +21,6 @@ public class JwtUtils {
         return Jwts.builder()
                 .subject(name)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(getSigningKey())
                 .compact();
     }
